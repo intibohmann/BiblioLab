7ae9ba7c5e2cad7152c2fc20be04d5fe3cda5b58
intibohmann <163604951+intibohmann@users.noreply.github.com>
2025-06-10 13:47:50 -0300

    Delete PPO---Biblioteca-Dirigida-main directory
    Delete de arquivos duplicados

--- PPO---Biblioteca-Dirigida-main/src/Biblioteca/salvar_categoria.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once("../Class/Categorias.class.php");
-try {
-    $categoria = new Categoria(null, $_POST['nome'], $_POST['descricao']);
-    $categoria->inserir();
-    header("Location: ../inicio/index.php");
-} catch (Exception $e) {
-    echo "Erro: " . $e->getMessage();
-}
